7647a69476da78c4196b1c50f843d2450f5ad91f
typhonrt <support@typhonjs.io>
2023-05-26 19:40:24 -0700

    initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import typescript          from '@rollup/plugin-typescript';
+import dts                 from 'rollup-plugin-dts';
+
+// Produce sourcemaps or not.
+const sourcemap = true;
+
+export default () =>
+{
+   return [
+      {   // This bundle is for the Node distribution.
+         input: 'src/index.ts',
+         output: [{
+            file: './dist/index.js',
+            format: 'es',
+            generatedCode: { constBindings: true },
+            sourcemap,
+         }],
+         plugins: [
+            typescript({ include: ['src/**/*'] })
+         ]
+      },
+
+      {   // This bundle is for bundled types.
+         input: 'src/index.ts',
+         output: [{
+            file: `./types/index.d.mts`,
+            format: 'es',
+            sourcemap: false
+         }],
+         plugins: [
+            typescript({ include: ['src/**/*'], sourceMap: false, inlineSources: false }),
+            dts()
+         ]
+      }
+   ];
+};
