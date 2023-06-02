6390d37b18982e41beec7e60c9077d4d441a4b0c
typhonrt <support@typhonjs.io>
2023-06-02 01:39:16 -0700

    Export types

--- src/trie/index.ts
+++ src/trie/index.ts
@@ -1,3 +1,3 @@
-export *       from './TrieSearch';
-export *       from './reducers';
-export type *  from './types';
+export * from './TrieSearch';
+export * from './reducers';
+export * from './types';
