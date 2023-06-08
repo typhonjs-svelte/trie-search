c622ed29971f4d4be7a4cf51c445ca33d0d7fff4
typhonrt <support@typhonjs.io>
2023-06-08 15:09:46 -0700

    restructure / rename tests.

---
+++ test/trie/TrieAPIGeneral.test.ts
@@ -0,0 +1,128 @@
+/**
+ * Provides general API testing / fuzzing to hit all code paths. The main API functionality testing is in
+ * `TrieSearch.test.ts`.
+ */
+
+
+import {TrieSearch} from "../../src";
+
+describe('TrieSearch - General tests', () =>
+{
+   describe('new TrieSearch(keyFields) should work', () =>
+   {
+      it('should set keyfields (1)', () =>
+      {
+         const ts = new TrieSearch(['key']);
+         expect(ts.keyFields.length).to.equal(1);
+      });
+
+      it('should set keyfields (2)', () =>
+      {
+         const ts = new TrieSearch('key');
+         expect(ts.keyFields.length).to.equal(1);
+      });
+   });
+
+   describe('destroy() / size accessor should work', () =>
+   {
+      const ts = new TrieSearch(['key']);
+
+      ts.add({ key: 'test' });
+
+      it('size accessor should be `4`', () => expect(ts.size).to.equal(4));
+
+      it('destroy() should reset data', () =>
+      {
+         ts.destroy();
+
+         expect(ts.isDestroyed).to.be.true;
+         expect(ts.size).to.equal(0);
+      });
+   });
+
+   describe('Hit all branches / argument guards', () =>
+   {
+      const ts = new TrieSearch('key');
+
+      it('empty add() has no effect', () =>
+      {
+         expect(ts.size).to.equal(0);
+         ts.add();
+         expect(ts.size).to.equal(0);
+      });
+
+      it('empty search() has no effect', () =>
+      {
+         expect(ts.size).to.equal(0);
+
+         // @ts-expect-error
+         expect(ts.search()).to.deep.equal([]);
+      });
+   });
+
+   describe('This Return Values', () =>
+   {
+      const ts = new TrieSearch('key');
+
+      it('add(...) should return this', () => expect(ts.add({ key: 'test' })).to.equal(ts));
+
+      it('clear(...) should return this', () => expect(ts.clear()).to.equal(ts));
+
+      it('map(...) should return this', () => expect(ts.map('key', { key: 'val' })).to.equal(ts));
+
+      it('destroy(...) should return this', () => expect(ts.destroy()).to.equal(ts));
+   });
+
+   describe('Subscriber callbacks', () =>
+   {
+      const ts = new TrieSearch('key');
+
+      let i = 0;
+
+      ts.subscribe(({ action, trieSearch }) =>
+      {
+         switch(i++)
+         {
+            case 0:
+               it('subscribe(...) should fire event', () =>
+               {
+                  expect(action).to.equal('subscribe');
+                  expect(trieSearch).to.equal(ts);
+               });
+               break;
+
+            case 1:
+               it('add(...) should fire event', () =>
+               {
+                  expect(action).to.equal('add');
+                  expect(trieSearch).to.equal(ts);
+               });
+               break;
+
+            case 2:
+               it('clear(...) should fire event', () =>
+               {
+                  expect(action).to.equal('clear');
+                  expect(trieSearch).to.equal(ts);
+               });
+               break;
+
+            case 3:
+               it('destroy(...) should fire event', () =>
+               {
+                  expect(action).to.equal('destroy');
+                  expect(trieSearch).to.equal(ts);
+               });
+               break;
+         }
+      });
+
+      // The following will fire events. --------------------------------------------------------------------------
+
+      ts.add({ key: 'test' });
+
+      ts.clear();
+
+      ts.destroy();
+   });
+});
