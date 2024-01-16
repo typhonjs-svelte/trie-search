75e55e08f6751f87b499fcc62d5a2be4b8d2be50
typhonrt <support@typhonjs.io>
2024-01-16 10:00:45 -0800

    Add subscription tests.

--- test/query/TrieSearchQuery.test.ts
+++ test/query/TrieSearchQuery.test.ts
@@ -47,5 +47,84 @@ describe('TrieSearchQuery', () =>
 
          assert.deepEqual([...tsq], []);
       });
-   })
+   });
+
+   describe('Basic Queries (subscription)', () =>
+   {
+      const ts = new TrieSearch<Item>('id');
+      const items = [
+         { id: 1, value: '1'},
+         { id: 2, value: '2'},
+         { id: 3, value: '3'}
+      ];
+
+      ts.add(items);
+
+      it('Has data', () =>
+      {
+         const tsq = new TrieSearchQuery<Item>(ts);
+         let count = 0;
+
+         tsq.subscribe(() =>
+         {
+            switch (count)
+            {
+               case 0:
+                  assert.deepEqual([...tsq], []);
+                  break;
+
+               case 1:
+                  assert.deepEqual([...tsq], [{ id: 1, value: '1' }]);
+                  break;
+
+               case 2:
+                  assert.deepEqual([...tsq], [{ id: 1, value: '1' }, { id: 2, value: '2' }]);
+            }
+            count++;
+         })
+
+         tsq.query.set('1');
+         tsq.query.set(['1', '2']);
+
+         assert.equal(count, 3);
+      });
+
+      it('Receives notification', () =>
+      {
+         const tsq = new TrieSearchQuery<Item>(ts);
+         let count = 0;
+
+         tsq.subscribe(() =>
+         {
+            switch (count)
+            {
+               case 0:
+                  assert.deepEqual([...tsq], []);
+                  break;
+
+               case 1:
+                  assert.deepEqual([...tsq], [{ id: 1, value: '1' }]);
+                  break;
+
+               case 2:
+                  assert.deepEqual([...tsq], [{ id: 1, value: '1' }, { id: 11, value: '11' }]);
+                  break;
+
+               case 3:
+                  assert.deepEqual([...tsq], []);
+                  break;
+            }
+
+            count++;
+         });
+
+         tsq.query.set('1');
+
+         ts.add({ id: 11, value: '11' });
+
+         ts.clear();
+
+         assert.equal(count, 4);
+      });
+   });
 });
